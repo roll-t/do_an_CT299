82261d898cdfd36c266041370546a82cf629e9cc
Thế Đạt <74842495+tdat1310@users.noreply.github.com>
2023-04-03 11:29:19 +0700

    cap nhat footer

--- footer.php
+++ footer.php
@@ -24,59 +24,61 @@
             <div class="list-contact">
                 <div class="list-contact-items">
                     <li class="items-contact title">
-                        Information 
+                        KẾT NỐI
                     </li>
-                    <li class="items-contact"><a href="#">
-                        About Us
+                    <li class="items-contact" ><a href="#">
+                    <h2 class="logo"
+                    style="width:49%;background:#f6f1f1;padding:5px;"
+                    >Home<span style="color:#E99C2E;">Design</span></h2>
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Contact Us
+                        
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        News
+                        Follow us
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Store
+                        Instagram - Facebook - Twitter
                     </a></li>
                 </div>
                 <div class="list-contact-items">
                     <li class="items-contact title">
-                        Collections
+                        HOME DESIGN
                     </li>
                     <li class="items-contact"><a href="#">
-                        Wooden Chair
+                        Giới thiệu
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Royal Cloth Sofa
+                        Tuyển dụng
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Accent Chair
+                       Thẻ hội viên
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Bed
+                        Đổi trả hàng
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Hanging Lamp
+                        Tổng công ty
                     </a></li>
                 </div>
                 <div class="list-contact-items">
                     <li class="items-contact title">
-                        My Accounts
+                        CẢM HỨNG
                     </li>
                     <li class="items-contact"><a href="#">
-                        My Account
+                        Sản Phẩm
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Wishlist
+                        Ý tưởng
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Community
+                        Phong cách
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        Order History
+                        Thẩm mỹ
                     </a></li>
                     <li class="items-contact"><a href="#">
-                        My Cart
+                        Tinh tế
                     </a></li>
                 </div>
                 <div class="list-contact-items">
@@ -84,8 +86,14 @@
                         Newsletter
                     </li>
                     <li class="items-contact"><a href="#">
-                        Subscribe to get latest news,update and information
+                    Hãy để lại email của bạn để nhận được những ý tưởng trang trí mới và những thông tin, ưu đãi từ Home design
                     </a></li>
+                    <li class="items-contact"><a href="#">
+                    Email: homedesign@company.com.vn
+                    </a></li>
+                    <li class="items-contact">
+                    Hotline: <span style='font-weight:bold;'>18007200</span>
+                    </li>
                     <li class="items-contact">
                         <div class="letter">
                             <input class="input-letter" type="text" placeholder="Enter Email Here...">
@@ -103,5 +111,5 @@
                 <li class="icon-items"><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
                 <li class="icon-items"><a href="#"><ion-icon name="logo-behance"></ion-icon></a></li>
             </div>
-            <div class="text">©Copyright. Designed And Developed By Themesine</div>
+            <div class="text">© Copyright. Designed And Developed By Home Design</div>
     </footer>
